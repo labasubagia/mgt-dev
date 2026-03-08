984fef3a1e7d9243cf407a82a81a8ecd2a53f544
labasubagia <31754766+labasubagia@users.noreply.github.com>
2026-03-09 07:29:09 +0800

    Simplify show_url.sh and add WSL support
    Refactor script to always show localhost URL and add WSL check.

--- show_url.sh
+++ show_url.sh
@@ -1,8 +1,12 @@
-if [[ $(cat /proc/sys/kernel/osrelease) =~ "WSL" ]]; then
-    WSL_IP=$(ip addr show eth0 | grep "inet " | awk '{print $2}' | cut -d/ -f1);
-    echo "Dashboard url: https://$WSL_IP:8443";
-    echo "Add '$WSL_IP dev.mgt' to C:\Windows\System32\drivers\etc\hosts";
-else
-    echo "Dashboard url: https://127.0.0.1:8443";
-    echo "Add '127.0.0.1 dev.mgt' to /etc/hosts"
+#!/bin/sh
+echo "Dashboard url: https://127.0.0.1:8443"
+echo "Add '127.0.0.1 dev.mgt' to /etc/hosts"
+
+# Use grep to check for "microsoft" in a portable way
+if uname -a | grep -iq "microsoft"; then
+    echo "\nIf you are using WSL and above does not work, try the following:"
+    # In WSL, get the eth0 IP
+    WSL_IP=$(ip addr show eth0 | grep "inet " | awk '{print $2}' | cut -d/ -f1)
+    echo "Dashboard url: https://$WSL_IP:8443"
+    printf "Add '%s dev.mgt' to %s\n" $WSL_IP 'C:\Windows\System32\drivers\etc\hosts'
 fi
