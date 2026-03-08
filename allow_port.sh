664a811e31da314022b84d1e80b1a82528e1bd77
Laba Subagia <labasubagia22@gmail.com>
2026-03-09 03:39:10 +0800

    Update port configuration and add supervisord setup for service management

--- allow_port.sh
+++ allow_port.sh
@@ -1,3 +1,1 @@
-sudo sysctl -w net.ipv4.ip_unprivileged_port_start=22
-# sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80 &&
-# sudo sysctl -w net.ipv4.ip_unprivileged_port_start=443
+sudo sysctl -w net.ipv4.ip_unprivileged_port_start=22;
